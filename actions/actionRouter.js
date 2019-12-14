e7a087786e71bdf5c14d050c093da2118f7e1038
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-12-13 20:25:36 -0500

    Added endpoint to add an action

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -4,8 +4,35 @@ const router = express.Router({
   mergeParams: true
 });
 
+// insert
+// update
+// remove
+
 router.get("/:actionId", (req, res, next) => {
   db.get(req.params.actionId)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.post("/", (req, res, next) => {
+  if (!req.body) {
+    return res.status(400).json({
+      message: "Empty input fields."
+    });
+  }
+
+  const newAction = {
+    project_id: req.params.id,
+    description: req.body.description,
+    notes: req.body.notes,
+    completed: false
+  };
+
+  db.insert(newAction)
     .then(data => {
       res.status(200).json(data);
     })

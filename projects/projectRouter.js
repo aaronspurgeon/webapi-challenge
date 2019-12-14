9feaed9ae5f357d2b5951bcf46666ae61744aa4b
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-12-13 19:47:00 -0500

    Added a post endpoint for a project

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -18,4 +18,26 @@ router.get("/", (req, res, next) => {
     });
 });
 
+router.post("/", (req, res, next) => {
+  if (!req.body) {
+    return res.status(400).json({
+      message: "Missing input fields"
+    });
+  }
+
+  const newProject = {
+    name: req.body.name,
+    description: req.body.description,
+    completed: false
+  };
+
+  db.insert(newProject)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
 module.exports = router;
